a7a0a0089010d507746de5daef618865fbb866df
Zuribaby <chiamakanwobodo07@gmail.com>
2023-07-22 20:02:44 -0700

    hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,13 +2,16 @@
 # 0-hello_route module
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    # Route definition
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
+    # App entering point
     app.run(host='0.0.0.0', port=5000)
