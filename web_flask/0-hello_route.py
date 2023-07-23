ea03a3c14a30690f5feda1f62d522c2a6d08ce36
Zuribaby <chiamakanwobodo07@gmail.com>
2023-07-22 19:55:37 -0700

    hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 # 0-hello_route module
-# A script that renders a page.
-
 from flask import Flask
 
 app = Flask(__name__)
 
 
-# Route definition
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
